ce259df41f4220a132c8396e45ff67be30c3831c
abhinavReja <183062682+abhinavReja@users.noreply.github.com>
2025-01-20 01:36:51 -0700

    Detect the crisis keyword and respond accordingly

---
+++ Data/keywords.py
@@ -0,0 +1,13 @@
+
+CRISIS_KEYWORDS = [
+    "suicide",
+    "hurt myself",
+    "kill myself",
+    "end my life",
+    "self-harm",
+    "cutting",
+    "die",
+    "worthless",
+    "no way out",
+    "give up",
+]
